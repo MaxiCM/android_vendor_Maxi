e0f6d306f5b49b09b10161e478d12f56cc2df691
Epirex <estebanguzzo@gmail.com>
2015-10-11 22:55:36 -0300

    Use Material sounds

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -10,8 +10,8 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/maxi/overlay/dictionaries
 
 # Default notification/alarm sounds
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=Argon.ogg \
-    ro.config.alarm_alert=Helium.ogg
+    ro.config.notification_sound=Ceres.ogg \
+    ro.config.alarm_alert=Carbon.ogg
 
 ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
     PRODUCT_COPY_FILES += \
