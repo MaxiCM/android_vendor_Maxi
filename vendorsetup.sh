fb285cd1cc1f2bfcf5b5a25b723d1cba6c60e1ca
Andres Mora Montero <andresmora1676@gmail.com>
2015-11-30 21:19:59 -0600

    Delete lt02wifi

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,6 @@
 add_lunch_combo maxi_jagnm-userdebug
 add_lunch_combo maxi_jag3gds-userdebug
 add_lunch_combo maxi_janice-userdebug
-add_lunch_combo maxi_lt02wifi-userdebug
 add_lunch_combo maxi_mako-userdebug
 add_lunch_combo maxi_otus-userdebug
 
