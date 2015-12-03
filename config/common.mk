5a0ba74f6dba5eb770e25ef7cc91e1ea91f823a0
Andres Mora Montero <andresmora1676@gmail.com>
2015-12-03 13:29:03 -0600

    Add copy files for tweaks
    Copy files MaxiCM Tweaks
    Tnks @Krizthian

--- config/common.mk
+++ config/common.mk
@@ -85,6 +85,7 @@ PRODUCT_COPY_FILES += \
 
 # MaxiCM Tweaks
 PRODUCT_COPY_FILES += \
+    vendor/maxi/prebuilt/common/etc/init.d/S58ramscript:system/etc/init.d/S58ramscript \
     vendor/maxi/prebuilt/common/etc/init.d/Zipaling:system/etc/init.d/Zipaling \
     vendor/maxi/prebuilt/common/etc/init.d/06removecache:system/etc/init.d/06removecache \
     vendor/maxi/prebuilt/common/etc/init.d/16sqlite_optimize:system/etc/init.d/16sqlite_optimize \
@@ -95,6 +96,7 @@ PRODUCT_COPY_FILES += \
     vendor/maxi/prebuilt/common/etc/init.d/LagFixer:system/etc/init.d/LagFixer \
     vendor/maxi/prebuilt/common/etc/init.d/LoopySmoothness:system/etc/init.d/LoopySmoothness \
     vendor/maxi/prebuilt/common/etc/init.d/Speedy:system/etc/init.d/Speedy \
+    vendor/maxi/prebuilt/common/etc/init.d/Ssmoothness_tweak:system/etc/init.d/Ssmoothness_tweak 
 
 ifneq ($(TARGET_BUILD_VARIANT),user)
 # userinit support
