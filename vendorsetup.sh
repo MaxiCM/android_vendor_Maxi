a07d81d7ff69a3e067fdf3e25a80f6866b97559f
Cristian Ortiz <cristian.2522@hotmail.com>
2015-12-04 19:13:53 -0500

    Add falcon to vendorsetup.sh
    Signed-off-by: Krizthian <cristianortizmorales2013@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
-add_lunch_combo maxi_otus-userdebug
+add_lunch_combo maxi_falcon-userdebug
 add_lunch_combo maxi_jagnm-userdebug
+add_lunch_combo maxi_jag3gds-userdebug
 add_lunch_combo maxi_janice-userdebug
+add_lunch_combo maxi_mako-userdebug
