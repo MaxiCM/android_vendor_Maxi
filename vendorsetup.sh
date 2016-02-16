37edf18108112d3bc661b6b7bc44cbf1b4acabcc
Andres Mora Montero <andresmora1676@gmail.com>
2016-02-16 13:29:06 -0600

    Add osprey

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,4 @@ add_lunch_combo maxi_jagnm-userdebug
 add_lunch_combo maxi_jag3gds-userdebug
 add_lunch_combo maxi_janice-userdebug
 add_lunch_combo maxi_mako-userdebug
+add_lunch_combo maxi_osprey-userdebug
