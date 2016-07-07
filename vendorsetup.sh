e0858d098f43a266c8959a2ca8384aa2cdd49aa4
Kevin Nuñez Banderas <kn06497@hotmail.com>
2016-07-07 10:37:13 -0500

    revert

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 for combo in $(curl -s https://raw.githubusercontent.com/MaxiCM/hudson/master/maxi-build-targets | sed -e 's/#.*$//' | grep maxi-6.0 | awk '{printf "maxi_%s-%s\n", $1, $2}')
 do
-    add_lunch_combo maxi_serranoltexx-userdebug
+    add_lunch_combo $combo
 done
